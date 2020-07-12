7fd947637eb45b9069ccdce02527beea9114dc6e
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-12 20:51:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,17 +3,12 @@ class Shoe
 
   BRANDS = []
 
-  # def initialize(brand)
-  #   @brand = brand
-  # end
+  def initialize(brand)
+    @brand = brand
+  end
 
   def brand=(brand)
     @brand = brand
     BRANDS.push(brand)
   end
-
-  # def cobble
-  #   self.condition = "new"
-  #   puts "Your shoe is as good as new!"
-  # end
 end
